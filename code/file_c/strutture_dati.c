df1af28a7f5229d6cd79005ca2140bd8d0f1d1a5
RichSenes <richsenes@gmail.com>
2024-03-01 12:27:24 +0100

    Definizione Strutture Dati Iniziali
    Definizione di alcune strutture dati che potrebbero essere utili per la creazione del progetto. Rappresentazione degli elementi "Rana", "Coccodrillo", "Pianta", "Proiettile" e "Griglia di gioco".

--- code/file_c/strutture_dati.c
+++ code/file_c/strutture_dati.c
@@ -1,2 +1,41 @@
 // File per la definizione di strutture dati da utilizzare nel gioco,
 // come lo stato della partita e le informazioni di gioco.
+
+// Struttura rappresentante la rana
+typedef struct {
+    int x; // coordinata x della posizione
+    int y; // coordinata y della posizione
+    int lifes; // numero di vite rimaste
+    int points; // punteggio del giocatore
+} Frog;
+
+// Struttura rappresentante un coccodrillo
+typedef struct {
+    int x; // coordinata x della posizione
+    int y; // coordinata y della posizione
+    int direction; // direzione del movimento
+    int type; // tipo di coccodrillo (buono o cattivo)
+} Crocodile;
+
+// Struttura rappresentante una piante
+typedef struct {
+    int x; // coordinata x della posizione
+    int y; // coordinata y della posizione
+    int shoot_frequency; // frequenza di sparo dei proiettili
+} Plant;
+
+// Struttura rappresentante un proiettile
+typedef struct {
+    int x; // coordinata x della posizione
+    int y; // coordinata y della posizione
+    int direction; // direzione del movimento
+    int speed; // velocita' del movimento
+} Bullet;
+
+// Struttura rappresentante la griglia di gioco
+#define GRID_DIM_X 20 // dimensione della griglia - asse x
+#define GRID_DIM_Y 10 // dimensione della griglia - asse y
+typedef struct {
+    // matrice bidimensionale di caratteri per la griglia di gioco
+    char cells[GRID_DIM_X] [GRID_DIM_Y];
+} Grid;
